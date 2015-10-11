a0ef3717e5899806d323b39fa8bd3ce1dddd62cb
Trever Shick <trevershick@gmail.com>
2015-10-11 13:07:28 -0400

    ignore the 503 from the health call

--- src/main/webapp/scripts/components/interceptor/errorhandler.interceptor.js
+++ src/main/webapp/scripts/components/interceptor/errorhandler.interceptor.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var ignoreHealth503 = function(response) {
+  return (response.data && response.data.status === 'DOWN' && response.status === 503);
+};
+var ignoreAccount401 = function(response) {
+  return (response.status == 401 && response.data.path.indexOf("/api/account") == 0);
+};
+var ignoreHttpErrors = [ ignoreHealth503, ignoreAccount401 ];
+
 angular.module('shikenApp')
-    .factory('errorHandlerInterceptor', function ($q, $rootScope) {
-        return {
-            'responseError': function (response) {
-                if (!(response.status == 401 && response.data.path.indexOf("/api/account") == 0 )){
-	                $rootScope.$emit('shikenApp.httpError', response);
-	            }
-                return $q.reject(response);
-            }
-        };
-    });
+.factory('errorHandlerInterceptor', function ($q, $rootScope) {
+  return {
+    'responseError': function (response) {
+      var ignore = _.some(ignoreHttpErrors, function(predicate) { return predicate(response); });
+      if (!ignore) {
+        $rootScope.$emit('shikenApp.httpError', response);
+      }
+      return $q.reject(response);
+    }
+  };
+});
